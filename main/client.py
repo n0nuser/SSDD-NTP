386e57e25d7030b3673da96b4b43f2cd5ca1a796
n0nuser <pjgr2000@gmail.com>
2022-04-30 16:29:46 +0200

    Upload Poetry.Lock and Requirements.txt

--- main/client.py
+++ main/client.py
@@ -1,27 +1,38 @@
 import requests
-from .models import Atleta
+import time
 
-numAtletas = 4
-HOST = "127.0.0.1"
-PORT = 8080
-FICHERO = "resultados.log"
+HOST = ["127.0.0.1", "192.168.1.24", "192.168.1.50"]
+PORT = [8080]
+DIRECCIONES = []
+for host in HOST:
+    for port in PORT:
+        DIRECCIONES.append("http://" + host + ":" + str(port) + "/")
+
+offset = lambda t0, t1, t2, t3: (t1 - t0 + t2 - t3) / 2
+delay = lambda t0, t1, t2, t3: (t1 - t0 + t3 - t2)
 
 
-def start():
-    reinicio = requests.get("http://" + HOST + ":" + str(PORT) + "/reinicio")
+def main():
+    mejorPar = {"address": None, "d": 999999, "o": 999999}
+    for direccion in DIRECCIONES:
+        print("\nProbando " + direccion)
+        print("{:^10} {:^25} {:^25}".format("Iteracion", "Offset", "Delay"))
+        for i in range(8):
+            t0 = time.time()
+            data = requests.get(direccion + "tiempo")
+            data = data.json()
+            t1 = data["t1"]
+            t2 = data["t2"]
+            t3 = time.time()
+            o = round(offset(t0, t1, t2, t3), 18)
+            d = round(delay(t0, t1, t2, t3), 18)
+            print("{:^10} {:^25} {:^25}".format(i + 1, o, d))
+            if d < mejorPar["d"]:
+                mejorPar["address"] = direccion
+                mejorPar["d"] = d
+                mejorPar["o"] = o
+    print("\nMejor par:" + str(mejorPar))
 
-    atletas = []
-    for num in range(numAtletas):
-        atletas.append(Atleta(dorsal="10" + str(num)))
 
-    for atleta in atletas:
-        atleta.start()
-    print("Atletas -> Hilos START")
-
-    for atleta in atletas:
-        atleta.join()
-
-    with open(FICHERO, "a+") as f:
-        resultados = requests.get("http://" + HOST + ":" + str(PORT) + "/resultados")
-        print(resultados.text)
-        f.write(resultados.text)
+if "__main__" == __name__:
+    main()
